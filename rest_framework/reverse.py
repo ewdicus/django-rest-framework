e329728fa46957568777d21f4019681a5ab53031
Elliott <ewdicus@gmail.com>
2015-03-26 13:09:49 -0700

    Revert "Merge branch 'feature/url-versions' of https://github.com/readevalprint/django-rest-framework"
    This reverts commit ac623c3e839618dcac24a1d6e24a38878f4921c5, reversing
    changes made to 2a27674a7971a60050d4530a0852a864b2065adb.

--- rest_framework/reverse.py
+++ rest_framework/reverse.py
@@ -4,9 +4,6 @@ Provide reverse functions that return fully qualified URLs
 from __future__ import unicode_literals
 from django.core.urlresolvers import reverse as django_reverse
 from django.utils.functional import lazy
-from django.core.urlresolvers import resolve
-from django.http import Http404
-
 
 
 def reverse(viewname, args=None, kwargs=None, request=None, format=None, **extra):
@@ -17,25 +14,9 @@ def reverse(viewname, args=None, kwargs=None, request=None, format=None, **extra
     if format is not None:
         kwargs = kwargs or {}
         kwargs['format'] = format
-
-    if request:
-        if hasattr(request, 'resolver_match'):
-            namespace = request.resolver_match.namespace
-        else:
-            try:
-                namespace = resolve(request.path).namespace
-            except Http404:
-                namespace=None
-
-        if namespace and ':' not in viewname:
-            viewname = '{namespace}:{viewname}'.format(namespace=namespace,
-                                                   viewname=viewname)
-
-    url = django_reverse(viewname, args=args, kwargs=kwargs,
-                          **extra)
+    url = django_reverse(viewname, args=args, kwargs=kwargs, **extra)
     if request:
         return request.build_absolute_uri(url)
-
     return url
 
 
